59e78201ffe46ab9f6dd2e46dc252d99e2feeaf8
Ubuntu <ubuntu@ip-172-31-68-27.ec2.internal>
2018-09-03 00:15:55 +0000

    changes

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-mkdir rr
-cd rr
-sudo apt update && sudo apt install -y bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev jdk-8-openjdk tmux
+mkdir ~/rr
+cd ~/rr
+sudo apt update && sudo apt install -y bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev openjdk-8-jdk tmux
+mkdir ~/bin
 curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
 repo init -u https://github.com/ResurrectionRemix/platform_manifest.git -b oreo --depth=1
